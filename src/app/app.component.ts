173c6ebe3aebea5977cf06fbf2ddb52aba4fccc5
Shruti2906 <65824891+Shruti2906@users.noreply.github.com>
2023-03-24 20:08:25 +0530

    employee CRUD

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,135 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'frontend';
+export class AppComponent implements OnInit{
+
+  id: any;
+  name: any;
+
+  employees: any[] = [];
+
+  constructor(private http: HttpClient) {}
+
+  ngOnInit(): void {
+   this.get()
+  }
+
+  get(): void{
+    this.http.get("http://localhost:3000/get").subscribe((res: any) => {
+      console.log('get called res', res);
+      this.employees = res;
+
+      // for (let i = 0; i < this.employee s.length; i++) {
+      //   const employee = this.employees[i];
+      //   console.log(`Name: ${employee.name}, Age: ${employee.age}, Position: ${employee.position}`);
+      // }
+
+    }, (err: any) => {
+      console.log('error', err);
+    });
+  }
+
+  add(): void {
+    const employee = {
+      id: this.id,
+      name: this.name
+    };
+
+    this.http.post("http://localhost:3000/add", employee).subscribe(response => {
+      console.log(response);
+         this.get()
+
+    }, error => {
+      console.error(error);
+    });
+   // this.get()
+   this.id = null;
+   this.name = '';
+
+  }
+
+  update() : void{
+    const employee = {
+      id: this.id,
+      name: this.name
+    };
+    this.http.put("http://localhost:3000/put", employee).subscribe(response => {
+      console.log(response);
+    }, error => {
+      console.error(error);
+    });
+    this.get()
+  }
+
+  deleteEmp() : void{
+
+    const employee = {
+      id: this.id,
+      name: 'z'
+    };
+
+    const id = this.id;
+
+    this.http.delete(`http://localhost:3000/delete/${this.id}`).subscribe(response => {
+      console.log(response);
+      this.get()
+    }, error => {
+      console.error(error);
+    });
+
+  }
+
 }
+
+
+// import { Component, OnInit } from '@angular/core';
+// import { HttpClient } from '@angular/common/http';
+// @Component({
+//   selector: 'app-root',
+//   templateUrl: './app.component.html',
+//   styleUrls: ['./app.component.css']
+// })
+// export class AppComponent implements OnInit{
+
+//   id: any
+//   name: any
+
+//   employees: any[] = [];
+
+//   constructor(private http: HttpClient){
+
+//   }
+//   ngOnInit(): void {
+//     // throw new Error('Method not implemented.');
+//     this.http.get("http://localhost:3000/get").subscribe((res: any) => {
+//       console.log('res', res);
+//       this.employees = res;
+
+//     }, (err: any)=>{
+//       console.log('error',err)
+
+//     })
+
+
+//     // this.http.post()
+
+//   }
+
+//   function add() {
+//     const employee = {
+//       id: this.id,
+//       name: this.name
+//     }
+
+//     this.employees.push(employee)
+
+//     this.id = null;
+//     this.name = "";
+//   }
+
+// }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,8 +1,13 @@
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
+// import { HttpClientModule} from '@angular/common';
+import { HttpClientModule} from '@angular/common/http';
+import { Component, OnInit } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+
 
 @NgModule({
   declarations: [
@@ -10,7 +15,9 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
